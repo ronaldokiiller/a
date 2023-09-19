dc93c5b9eaa83796f9ea980bfbe725462cad3205
Ronaldo Kiiller <ronaldo.kiiller@escola.pr.gov.br>
2023-09-19 13:02:49 -0300

    aula 5 refatorado melhor organização docstrings

--- apps/receitas/views/receita.py
+++ apps/receitas/views/receita.py
@@ -6,12 +6,12 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
-    paginator = Paginator(receitas, 3)
+    paginator = Paginator(receitas, 6)
     page = request.GET.get('page')
     receitas_por_pagina = paginator.get_page(page)
 
     dados = {
-        'receitas' : receitas
+        'receitas' : receitas_por_pagina
     }
     return render(request,'receitas/index.html', dados)
 
